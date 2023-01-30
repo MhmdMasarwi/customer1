291943b7e0530b4e37bf4560375c78b2499be1ed
Ali Esa <aliesa059@gmail.com>
2023-01-30 22:22:53 +0200

    add buttons

---
+++ store/src/Component/ProductPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ProductPage.css'
+function ProductPage() {
+  return (
+    <div className="Button" >
+        <button>phone </button>
+        <button> monitor</button>
+        <button> camera </button>
+    </div>
+  )
+}
+
+export default ProductPage
