1fe2e7e5f78d814b18d1f4a5af5a0c91f7578d2f
Ehab Melhem <Melhemehab@gmail.com>
2023-02-07 20:46:00 +0200

    setup add product and bootstrap css url

--- store/src/App.js
+++ store/src/App.js
@@ -2,15 +2,23 @@ import "./App.css";
 import ProductPage from "./Component/ProductPage";
 import Footer from "./Component/Footer";
 import { Routes, Route } from "react-router-dom";
-
+import AddProdect from "./Component/AddProdect";
 function HomePage() {
   return <div>this is home Page</div>;
+}
+function AddProductPage() {
+  return (
+    <div>
+      <AddProdect />
+    </div>
+  );
 }
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/addProduct" element={<AddProductPage />} />
       </Routes>
       <Footer />
       {/* <ProductPage/> */}
--- store/src/Component/AddProdect.js
+++ store/src/Component/AddProdect.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import './AddProdect.css'
+import React from "react";
+import "./AddProdect.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
-const AddProdect= () => {
+const AddProdect = () => {
   return (
-    <div>
-      
+    <div className="addproduct">
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Product name</Form.Label>
+          <Form.Control type="text" placeholder="Enter name" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Image name</Form.Label>
+          <Form.Control type="text" placeholder="Enter Url" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Category name</Form.Label>
+          <Form.Control type="text" placeholder="Enter Category" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
-  )
-}
+  );
+};
 
-export default AddProdect
+export default AddProdect;
